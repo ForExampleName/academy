e8f24f5d57d4a6e71587001a77ee474fac2b2a81
Piotr Zhukovetc <zhukovetc.petya@gmail.com>
2021-07-22 18:32:07 +0300

    Homework 2. Task 1

---
+++ academy/src/by/academy/homeworks/homework_2/SimilarStrings.java
@@ -0,0 +1,36 @@
+package by.academy.homeworks.homework_2;
+
+import java.util.Scanner;
+
+public class SimilarStrings {
+	private static final Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.print("Enter first word: ");
+		String firstWord = scanner.nextLine();
+		
+		System.out.print("Enter second word: ");
+		String secondWord = scanner.nextLine();
+		
+		if(firstWord.length() != secondWord.length()) {
+			System.out.println(false);
+			scanner.close();
+			return;
+		}
+		
+		int result1 = firstWord.charAt(0);
+		int result2 = secondWord.charAt(0);
+		
+		for(int i=1;i<firstWord.length();i++) {
+			result1 ^= firstWord.charAt(i);
+		}
+		
+		for(int i=1;i<secondWord.length();i++) {
+			result2 ^= secondWord.charAt(i);
+		}
+		
+		System.out.println(result1 == result2);
+		
+		scanner.close();
+	}
+}
