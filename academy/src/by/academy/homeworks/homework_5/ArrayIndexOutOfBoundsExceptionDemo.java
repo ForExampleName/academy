e034a1a7b49dd439e33740f6627597061059513b
Piotr Zhukovetc <zhukovetc.petya@gmail.com>
2021-08-19 13:59:52 +0300

    Homework 5 edited

--- academy/src/by/academy/homeworks/homework_5/ArrayIndexOutOfBoundsExceptionDemo.java
+++ academy/src/by/academy/homeworks/homework_5/ArrayIndexOutOfBoundsExceptionDemo.java
@@ -4,7 +4,7 @@ public class ArrayIndexOutOfBoundsExceptionDemo {
 
 	public static void main(String[] args) {
 		int[] array = new int[8];
-		
+
 		try {
 			for (int i = 0; i < 10; i++) {
 				array[i] = i;
--- academy/src/by/academy/homeworks/homework_5/LetterDictionary.java
+++ academy/src/by/academy/homeworks/homework_5/LetterDictionary.java
@@ -6,21 +6,21 @@ import java.util.Scanner;
 
 public class LetterDictionary {
 	private static Scanner scanner = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 		Map<Character, Integer> dictionary = new HashMap<>();
 		System.out.print("Enter your string:");
-		
+
 		String str = scanner.nextLine();
-		
-		for(Character letter: str.toCharArray()) {
+
+		for (Character letter : str.toCharArray()) {
 			dictionary.put(letter, dictionary.getOrDefault(letter, 0) + 1);
 		}
-		
-		for(Character key: dictionary.keySet()) {
+
+		for (Character key : dictionary.keySet()) {
 			System.out.println(key + " : " + dictionary.get(key) + " times");
 		}
-		
+
 		scanner.close();
 	}
 }
